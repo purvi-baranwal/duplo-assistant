54bba1f0f9e69e8a13c11fb50f1a63ababa6ce0d
aditya-kumar-wbd <aditya.kumar@wbd.com>
2025-06-03 15:18:58 +0530

    update

--- src/main/java/assistant/controller/AIAssistantController.java
+++ src/main/java/assistant/controller/AIAssistantController.java
@@ -2,15 +2,11 @@ package assistant.controller;
 
 import assistant.service.QueryExecutionService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:9090/index.html") // or *
 @RestController
 @RequestMapping("/assistant")
 public class AIAssistantController {
