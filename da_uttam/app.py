d9c940f35f1a52f25c847e6e3559681c33870231
aditya-kumar-wbd <aditya.kumar@wbd.com>
2025-06-17 13:05:08 +0530

    uploaded uttams assitant python code

---
+++ da_uttam/app.py
@@ -0,0 +1,79 @@
+import streamlit as st
+from langchain.vectorstores.chroma import Chroma
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.llms.ollama import Ollama
+from get_embedding_function import get_embedding_function
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+CHROMA_PATH = "/Users/adikumar/Desktop/da_uttam/db_chat"
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+___
+Answer the question based on the above context: {question}
+"""
+
+
+def query_rag(query_text: str):
+    embedding_function = get_embedding_function()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+    results = db.similarity_search_with_score(query_text, k=4)
+
+    # Combhine the results to pass llm
+
+    context_text = "\n\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+
+    model = Ollama(model="llama3.2")
+    response_text = model.invoke(prompt)
+    # print(context_text)  # Printing before giving it to llm
+
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    return response_text, sources
+
+
+st.title("Interactive Chatbot")
+st.write(
+    "This Chatbot will answer your question based on the document availble locally"
+)
+
+query_text = st.text_input("Enter your query:", placeholder="Type your question here..")
+if st.button("Submit") and query_text:
+    with st.spinner("Generating responseee..."):
+        try:
+            response, sources = query_rag(query_text)
+            st.success("response generated")
+            st.write(f"**Response:**{response}")
+            st.write(
+                f"**Sources:**{', '.join(str(source) for source in sources if source)}"
+            )
+
+        except Exception as e:
+            st.error(f"An error occured : {e}")
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    data = request.json
+   
+    query_text = data['text']
+    if not query_text:
+        return jsonify({'error': 'No query text provided'}), 400
+    result,sources = query_rag(query_text)
+    return jsonify({'result': result, "sources": sources})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ da_uttam/populate_database.py
@@ -0,0 +1,103 @@
+import argparse
+import os
+import shutil
+from langchain.document_loaders.pdf import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema.document import Document
+from langchain.vectorstores.chroma import Chroma
+from get_embedding_function import get_embedding_function
+
+
+CHROMA_PATH = "/Users/adikumar/Desktop/da_uttam/db_chat"
+DATA_PATH = "/Users/utgupta/Documents/LLM/data"
+DATA_PATH = "/Users/adikumar/Desktop/da_uttam/data"
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reset", action="store_true", help="Reset the database.")
+    args = parser.parse_args()
+    if args.reset:
+        print("Clearning Database")
+        clear_database()
+
+    documents = load_document()
+    chunks = split_documents(documents)
+    add_to_chroma(chunks)
+
+
+def load_document():
+    document_loader = PyPDFDirectoryLoader(DATA_PATH)
+    return document_loader.load()
+
+
+def split_documents(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1500,
+        chunk_overlap=250,
+        length_function=len,
+        # is_separater_regex=False,
+    )
+    return text_splitter.split_documents(documents)
+
+
+def add_to_chroma(chunks: list[Document]):
+    db = Chroma(
+        persist_directory=CHROMA_PATH, embedding_function=get_embedding_function()
+    )
+    chunks_with_ids = calculate_chunk_ids(chunks)
+
+    # add or update the docs
+
+    existing_items = db.get(include=[])
+    existing_ids = set(existing_items["ids"])
+    print(f"Number of existing documents in DB: {len(existing_ids)}")
+
+    # only add document that don't exist in DB
+    new_chunks = []
+    for chunk in chunks_with_ids:
+        if chunk.metadata["id"] not in existing_ids:
+            new_chunks.append(chunk)
+
+    if len(new_chunks):
+        print(f"adding new document: {len(new_chunks)}")
+        new_chunks_ids = [chunk.metadata["id"] for chunk in new_chunks]
+        db.add_documents(new_chunks, ids=new_chunks_ids)
+        db.persist()
+    else:
+        print("No new Document to add")
+
+
+def calculate_chunk_ids(chunks):
+    last_page_id = None
+    current_chunk_index = 0
+
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+
+        # if pageid is same as the last one increase the the index
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+
+        else:
+            current_chunk_index = 0
+
+        # calculate the chunk ids
+        chunk_id = f"{current_page_id}:{current_chunk_index}"
+        last_page_id = current_page_id
+
+        # add it to page metadata
+        chunk.metadata["id"] = chunk_id
+
+    return chunks
+
+
+def clear_database():
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+
+if __name__ == "__main__":
+    main()
