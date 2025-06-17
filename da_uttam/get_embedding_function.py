88af4d66f7cde368df18dff757d382933f78d880
aditya-kumar-wbd <aditya.kumar@wbd.com>
2025-06-17 14:11:24 +0530

    Delete da_uttam directory

--- da_uttam/get_embedding_function.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.embeddings.ollama import OllamaEmbeddings
-
-
-def get_embedding_function():
-    embeddings = OllamaEmbeddings(model="mxbai-embed-large")
-
-    return embeddings
